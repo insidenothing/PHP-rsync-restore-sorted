beee3e64dedb35502a10e519b814c4faf7ea8374
Patrick McGuire <insidenothing@gmail.com>
2011-01-19 20:35:37 -0800

    since sort is working removing -l

--- restore.php
+++ restore.php
@@ -1,6 +1,6 @@
 <?php
 function cliPause(){
-echo "Press enter to continue: ";
+echo "Press enter to continue";
 $handle = fopen ("php://stdin","r");
 $null = fgets($handle);
 echo "\n";
@@ -11,7 +11,7 @@ $line = fgets($handle);
 $path = trim($line);
 echo "sorting $path...\n";
 ob_start();
-$last_line = system('ls '.$path.' -t -l', $retval);
+$last_line = system('ls '.$path.' -t', $retval);
 $buffer = ob_get_clean();
 echo "Loaded \n";
 $lines = explode("
