c8398be48f822408063df5c9969772eb3c85cec7
Patrick McGuire <insidenothing@gmail.com>
2011-01-19 20:28:00 -0800

    attempt to echo

--- restore.php
+++ restore.php
@@ -1,4 +1,9 @@
 <?php
+function cliPause(){
+echo "Press enter to continue: ";
+$handle = fopen ("php://stdin","r");
+echo "\n";
+}
 echo "Path to sort and restore: ";
 $handle = fopen ("php://stdin","r");
 $line = fgets($handle);
@@ -12,5 +17,10 @@ $lines = explode("
 ", $buffer);
 $passes = count($lines);
 echo "Ready to process $passes files / folders \n";
-
+cliPause();
+$line=0;
+while($line < $passes){
+echo "$lines[$line] \n";
+}
+cliPause();
 ?>
