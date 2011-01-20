467553bdedf693edd552f1ce6e86313daef15148
Patrick McGuire <insidenothing@gmail.com>
2011-01-19 20:21:43 -0800

    see ho a buffer goes

--- restore.php
+++ restore.php
@@ -4,8 +4,13 @@ $handle = fopen ("php://stdin","r");
 $line = fgets($handle);
 $path = trim($line);
 echo "sorting $path...\n";
+ob_start();
 $last_line = system('ls '.$path.' -t -l', $retval);
-echo "Done \n";
-//$last_line;
-//$retval;
+$buffer = ob_get_clean();
+echo "Loaded \n";
+$lines = explode("
+", $buffer);
+$passes = count($lines);
+echo "Ready to process $passes files / folders \n";
+
 ?>
