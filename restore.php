4ace3195e89fc385a1fdfe5683a6ff681cb59892
Patrick McGuire <insidenothing@gmail.com>
2011-01-19 20:13:54 -0800

    hrmm

--- restore.php
+++ restore.php
@@ -1,12 +1,11 @@
 <?php
-echo "Are you sure you want to do this?  Type 'yes' to continue: ";
+echo "Path to sort and restore: ";
 $handle = fopen ("php://stdin","r");
 $line = fgets($handle);
 $path = trim($line);
-echo "Using $path...\n";
+echo "sorting $path...\n";
 $last_line = system('ls '.$path.' -t -l', $retval);
-echo '
-</pre>
-<hr />Last line of the output: ' . $last_line . '
-<hr />Return value: ' . $retval;
+echo "Done \n";
+//$last_line;
+//$retval;
 ?>
