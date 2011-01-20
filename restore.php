3bd26ff7bde72d6ffc1d8a531a93c1fe63df2564
Patrick McGuire <insidenothing@gmail.com>
2011-01-19 19:53:34 -0800

    base

--- restore.php
+++ restore.php
@@ -2,3 +2,33 @@
 // placeholder
 
 ?>
+
+
+<hr>
+Session Data:
+<table>
+<tr>
+<td>user</td>
+<td><input></td>
+</tr>
+<tr>
+<td>password</td>
+<td><input></td>
+</tr>
+<tr>
+<td>folder</td>
+<td><input></td>
+</tr>
+<tr>
+<td>sort order</td>
+<td><input></td>
+</tr>
+</table>
+<hr>
+Current Processing Order
+<table>
+<tr>
+<td>..</td>
+<td>..</td>
+</tr>
+</table>
