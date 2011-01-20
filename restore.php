854af6b9105a98e0f35577054fd8c62f60b05cf3
Patrick McGuire <insidenothing@gmail.com>
2011-01-19 19:58:10 -0800

    again

--- restore.php
+++ restore.php
@@ -8,4 +8,18 @@ if(trim($line) != 'yes'){
 }
 echo "\n";
 echo "Thank you, continuing...\n";
+
+
+
+echo "Are you sure you want to not do this?  Type 'no' to continue: ";
+$handle = fopen ("php://stdin","r");
+$line = fgets($handle);
+if(trim($line) != 'yes'){
+    echo "ABORTING!\n";
+    exit;
+}
+echo "\n";
+echo "Thank you, continuing...\n";
+
+
 ?>
